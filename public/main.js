a7bef0829304c87f864ce47470b22be14ee31c45
Tim Koopmans <tim.koops@gmail.com>
2023-07-25 20:46:55 +1000

    clean up static assets

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 const chartHeight = Math.floor(document.documentElement.clientHeight / 2);
 const resolution = 2000;
-
-const chart = LightweightCharts.createChart(document.getElementById('chart'), {
+const symbol = 'ETH';
+const chartOptions = {
     width: document.documentElement.clientWidth,
     height: chartHeight,
     timeScale: {
@@ -17,107 +17,98 @@ const chart = LightweightCharts.createChart(document.getElementById('chart'), {
         vertLines: { color: '#444' },
         horzLines: { color: '#444' },
     },
-});
+};
 
-// Setting the border color for the vertical axis
-chart.priceScale().applyOptions({
-    borderColor: "#71649C",
-});
-
-// Setting the border color for the horizontal axis
-chart.timeScale().applyOptions({
-    borderColor: "#71649C",
-});
-
+const chart = createChart('chart', chartOptions);
 const candlestickSeries = chart.addCandlestickSeries();
-
-const symbol = 'ETH'
 setupAutoRefresh();
 
-function updateData() {
-    fetch(`http://localhost:8000/data/${symbol}/60s`)
-        .then(response => response.json())
-        .then(data => {
-            const formattedData = data.map(datum => ({
-                time: datum.time_bucket * resolution / 1000 / 2,
-                open: datum.open_price,
-                high: datum.high_price,
-                low: datum.low_price,
-                close: datum.close_price,
-            }));
-            candlestickSeries.setData(formattedData);
-            chart.timeScale().fitContent();
-        });
+function createChart(id, options) {
+    const chart = LightweightCharts.createChart(document.getElementById(id), options);
+    setAxisColor(chart, '#71649C');
+    return chart;
+}
 
-    updateLiveTrades('ETH');
+function setAxisColor(chart, color) {
+    chart.priceScale().applyOptions({ borderColor: color });
+    chart.timeScale().applyOptions({ borderColor: color });
+}
+
+function fetchData(url) {
+    return fetch(url).then(response => response.json());
+}
+
+function updateData() {
+    fetchData(`http://localhost:8000/data/${symbol}/60s`)
+        .then(formatData)
+        .then(data => {
+            candlestickSeries.setData(data);
+            chart.timeScale().fitContent();
+        })
+        .catch(console.error);
+
+    updateLiveTrades(symbol);
+}
+
+function formatData(data) {
+    return data.map(datum => ({
+        time: datum.time_bucket * resolution / 1000 / 2,
+        open: datum.open_price,
+        high: datum.high_price,
+        low: datum.low_price,
+        close: datum.close_price,
+    }));
 }
 
 function setupAutoRefresh() {
-    // Call updateChartData initially
     updateData();
-
-    // Set up the auto-refresh interval
     setInterval(updateData, resolution);
 }
 
-// Function to fetch and append live trades to the container
 function updateLiveTrades(symbol) {
-    fetch(`http://localhost:8000/trades/${symbol}`)
-        .then(response => response.json())
-        .then(data => {
-            // Get the container element
+    fetchData(`http://localhost:8000/trades/${symbol}`)
+        .then(createTradeTable)
+        .then(table => {
             const tradeContainer = document.getElementById('tradeContainer');
-
-            // Clear the existing content in the container
             tradeContainer.innerHTML = '';
-
-            // Create a table to hold the trade data
-            const tradeTable = document.createElement('table');
-            tradeTable.classList.add('trade-table');
-
-            // Add table headers
-            const headerRow = tradeTable.insertRow();
-            const headers = ['ID', 'Time', 'Exchange', 'Base', 'Quote', 'Price', 'Quantity'];
-            headers.forEach(headerText => {
-                const headerCell = document.createElement('th');
-                headerCell.textContent = headerText;
-                headerRow.appendChild(headerCell);
-            });
-
-            // Append the new trade data to the table
-            for (let i = data.length - 1; i >= 0; i--) {
-                const datum = data[i];
-                const row = tradeTable.insertRow();
-
-                const timestamp = new Date(datum.timestamp).toLocaleString();
-
-                const cellId = row.insertCell();
-                cellId.textContent = datum.id;
-
-                const cellTimestamp = row.insertCell();
-                cellTimestamp.textContent = timestamp;
-
-                const cellExchange = row.insertCell();
-                cellExchange.textContent = datum.exchange;
-
-                const cellBase = row.insertCell();
-                cellBase.textContent = datum.base;
-
-                const cellQuote = row.insertCell();
-                cellQuote.textContent = datum.quote;
-
-                const cellPrice = row.insertCell();
-                cellPrice.textContent = datum.price;
-
-                const cellQty = row.insertCell();
-                cellQty.textContent = datum.qty;
-            }
-
-            // Append the table to the container
-            tradeContainer.appendChild(tradeTable);
+            tradeContainer.appendChild(table);
         })
-        .catch(error => {
-            console.error('Error fetching live trades:', error);
-        });
+        .catch(console.error);
 }
 
+function createTradeTable(data) {
+    const tradeTable = document.createElement('table');
+    tradeTable.classList.add('trade-table');
+    addHeaders(tradeTable, ['ID', 'Time', 'Exchange', 'Base', 'Quote', 'Price', 'Quantity']);
+    addRows(tradeTable, data);
+    return tradeTable;
+}
+
+function addHeaders(table, headers) {
+    const headerRow = table.insertRow();
+    headers.forEach(headerText => {
+        const headerCell = document.createElement('th');
+        headerCell.textContent = headerText;
+        headerRow.appendChild(headerCell);
+    });
+}
+
+function addRows(table, data) {
+    for (let i = data.length - 1; i >= 0; i--) {
+        const datum = data[i];
+        const row = table.insertRow();
+        const cells = [
+            datum.id,
+            new Date(datum.timestamp).toLocaleString(),
+            datum.exchange,
+            datum.base,
+            datum.quote,
+            datum.price,
+            datum.qty,
+        ];
+        cells.forEach(cellText => {
+            const cell = row.insertCell();
+            cell.textContent = cellText;
+        });
+    }
+}
